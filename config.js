bc9c2fabb013435960f2f29cd3dcdb470a946cd6
digitalsolutionssne-bit <digitalsolutionssne@gmail.com>
2026-05-29 22:54:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@
 // ==========================================
 const APP_CONFIG = {
 APP_NAME: "App Code Maintainer (SNE)",
-GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxrvR-INi3X6cVNG2GgccCHwm8WIK7yc7w_qEC5LNN1OuUBNk77aqSDuW5XZ5ryUwPgjQ/exec"
+GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxrvR-INi3X6cVNG2GgccCHwm8WIK7yc7w_qEC5LNN1OuUBNk77aqSDuW5XZ5ryUwPgjQ/exec",
+TARGET_REPO: "digitalsolutionssne-bit/Fail-Safe-Code-Updater",
+TARGET_FOLDER_ID: "1_vb2nSCTPfRj9lzcZcjTqYRMicIWHFqa",
+TARGET_BRANCH: "main"
 };
