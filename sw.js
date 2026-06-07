17a98c79a049416a1530d37e575bf391ba45b915
digitalsolutionssne-bit <digitalsolutionssne@gmail.com>
2026-06-07 09:04:03 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'fail-safe-updater-v1.1.4';
+self.addEventListener('install', (e) => {
+self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+e.waitUntil(clients.claim());
+});
